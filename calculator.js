baca56fbd3a3aecd5920d34210381bd968f735ba
Christofer Eliasson <mail@christofereliasson.eu>
2012-10-25 09:52:54 +0200

    Disable backspace button if calculator is showing a result. Improved code commenting.

--- calculator.js
+++ calculator.js
@@ -20,35 +20,46 @@
  */
 
 var Calculator = function () {
+
+    // Helper variable declarations
     var self = this,
         decimalMark = ".",
         sum = 0,
         prevOperator;
 
+    // Define default values
     self.display = ko.observable("0");
-    self.isSum = ko.observable(false);
+    self.isShowingResult = ko.observable(false);
 
     // Callback for each number button
     self.number = function (item, event) {   
         var button = event.target.innerText;
 
-        if (self.isSum()) {
+        // If a result has been shown, make sure we
+        // clear the display before displaying any new numbers
+        if (self.isShowingResult()) {
             self.clearDisplay();
-            self.isSum(false);
+            self.isShowingResult(false);
         }
 
         // Make sure we only add one decimal mark
         if (button == decimalMark && self.display().indexOf(decimalMark) > -1)
             return;
 
+        // Make sure that we remove the default 0 shown on the display
+        // when the user press the first number button
         var newValue = (self.display() === "0" && button != decimalMark) ? button : self.display() + button; 
-        self.display(newValue);            
+        // Update the display
+        self.display(newValue);
     };
 
     // Callback for each operator button
     self.operator = function (item, event) {
         var button = event.target.innerText;
-        if (!self.isSum()) {
+        // Only perform calculation if numbers
+        // has been entered since last operator button was pressed
+        if (!self.isShowingResult()) {
+            // Perform calculation
             switch (prevOperator) {
                 case "+":
                     sum = sum + parseFloat(self.display(), 10);
@@ -67,15 +78,26 @@ var Calculator = function () {
             };
         }
 
+        // Avoid showing a result until you have at least
+        // two terms to perform calculation on
         if (prevOperator)
             self.display(sum);
 
+        // Make sure we don't try to calculate with the equal sign
         prevOperator = (button === "=") ? null : button;
-        self.isSum(true);
+        // Always set the calculator into showing result state 
+        // after an operator button has been pressed
+        self.isShowingResult(true);
     };
 
     // Callback for each backspace button
     self.backspace = function (item, event) {
+        // Disable backspace if the calculator is shown a result
+        if (self.isShowingResult())
+            return;
+
+        // Remove the last character, and make the display zero when
+        // last character is removed
         if (self.display().length > 1) {
             self.display(self.display().substr(0, self.display().length - 1));
         } else {
@@ -83,17 +105,18 @@ var Calculator = function () {
         }
     };
 
-    // Clear the calculator
+    // Clear the entire calculator
     self.clear = function () {
         prevOperator = null;
         self.clearDisplay();
         sum = 0;
     };
 
-    // Clear the display
+    // Clear just the display
     self.clearDisplay = function () {
         self.display("0");
     };
 };
 
+// Let's do some magic!
 ko.applyBindings(new Calculator());
